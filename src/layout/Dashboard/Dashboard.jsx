5cb05de29efe2e065741e1d7ef7457c9287423f1
Tayeba Sultana <sultanatayeba43@gmail.com>
2025-05-04 14:11:20 +0600

    modify: modify dashboard sidebar

--- src/layout/Dashboard/Dashboard.jsx
+++ src/layout/Dashboard/Dashboard.jsx
@@ -20,7 +20,7 @@ const Dashboard = () => {
     return (
         <div className="flex">
           {/* Sidebar for large screens (left side) */}
-          <div className="lg:block hidden w-80 bg-[#859f3d] p-4">
+          <div className="lg:block hidden w-80 bg-[#859f3d] p-4 min-h-screen">
                 <ul className="menu text-base-content min-h-full">
                     {/* Sidebar content based on user role */}
                     {role === 'tourist' && (
