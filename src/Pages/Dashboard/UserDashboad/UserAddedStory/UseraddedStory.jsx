c2638052c4e0e1a7cd4ab93680e9897da349d855
Tayeba Sultana <sultanatayeba43@gmail.com>
2025-05-04 14:06:52 +0600

    modify: modify manage story page hadding

--- src/Pages/Dashboard/UserDashboad/UserAddedStory/UseraddedStory.jsx
+++ src/Pages/Dashboard/UserDashboad/UserAddedStory/UseraddedStory.jsx
@@ -5,7 +5,7 @@ import StoryCard from '../../../../components/Shared/StoryCard';
 const UserAddedStory = () => {
     return (
         <div className='text-center'>
-            <h1 className='text-4xl text-center mb-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>My Added Stories</h1>
+            <h1 className='text-4xl text-center mb-10'>My Added Stories</h1>
             <StoryCard />
         </div>
     );
